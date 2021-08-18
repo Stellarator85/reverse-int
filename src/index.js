6cb8fbf4b3bd70f94a25245aae20f66d55548630
Stellarator85 <86070933+Stellarator85@users.noreply.github.com>
2021-08-19 02:07:21 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n){
+	if (n >= 0)
 	return n.toString().split("").reverse().join("");
+	if (n < 0)
+	return ('-' + n.toString().split("").reverse().slice(0, -1).join("")); 
 }
+

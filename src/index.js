fc21f0049afd71544e819e8e09e0ba2cb049207f
Stellarator85 <86070933+Stellarator85@users.noreply.github.com>
2021-08-19 02:00:29 +0300

    fix: fix function

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
 module.exports = function reverse (n){
-	n = n.toString();
-	if (n == 0) return 0;
-	if (n > 0)
-	return n.split("").reverse().join("");
-	if (n < 0)
-	return ('-' + n.split("").reverse().slice(0, -1).join(""));
+	return n.toString().split("").reverse().join("");
 }

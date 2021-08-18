26eb8eff94f2b960960f1d36bf3afa027871f2e0
Stellarator85 <86070933+Stellarator85@users.noreply.github.com>
2021-08-18 16:56:38 +0300

    fix: fix function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-var rev_num = 0;
-var base_pos = 1;
-module.exports = function reverse(n)
-{
-    if(n > 0)
+
+module.exports = function reverse (n)
     {
-        reverse(Math.floor(n/10));
-        rev_num += (n%10)*base_pos;
-        base_pos *= 10;
+        let str
+            = n.toString().split("").reverse().join("");
+         
+        n = parseInt(str);
+ 
+        return str;
     }
-    return rev_num;
-};
